20790007c05a322d81aaf39049dc0060d73e45b5
Aymen Hachicha <aymenhachicha96@gmail.com>
2018-12-07 14:29:54 +0100

    added board and room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule
