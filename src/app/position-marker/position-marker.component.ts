33eacb4855411053facd074d6d473e470d556e37
Aymen Hachicha <aymenhachicha96@gmail.com>
2018-12-09 20:16:05 +0100

    created position marker component

---
+++ src/app/position-marker/position-marker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-position-marker',
+  templateUrl: './position-marker.component.html',
+  styleUrls: ['./position-marker.component.scss']
+})
+export class PositionMarkerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
