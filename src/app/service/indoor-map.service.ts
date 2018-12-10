dd81d343367ece351ae07328871bbc04fd201735
Aymen Hachicha <aymenhachicha96@gmail.com>
2018-12-10 03:13:49 +0100

    mocking data to show an example of a indoorMap

--- src/app/service/indoor-map.service.ts
+++ src/app/service/indoor-map.service.ts
@@ -9,9 +9,15 @@ export class IndoorMapService {
 
   getRooms() {
     return [
-      {name: 'Room 1', x: 0, y: 0, xdim: 50, ydim: 20},
-      {name: 'Room 2', x: 50, y: 50, xdim: 60, ydim: 30},
-      {name: 'Room 3', x: 100, y: 0, xdim: 40, ydim: 20},
+      {name: 'R100', x: 0, y: 0, xdim: 50, ydim: 40},
+      {name: 'R101', x: 50, y: 0, xdim: 55, ydim: 40},
+      {name: 'R102', x: 145, y: 0, xdim: 55, ydim: 40},
+      {name: 'R103', x: 0, y: 80, xdim: 50, ydim: 40, pos: 'top'},
+      {name: 'R104', x: 50, y: 80, xdim: 55, ydim: 40, pos: 'top'},
+      {name: 'R105', x: 145, y: 80, xdim: 55, ydim: 40, pos: 'top'},
+      {name: 'R106', x: 0, y: 160, xdim: 50, ydim: 40, pos: 'top'},
+      {name: 'R107', x: 50, y: 160, xdim: 55, ydim: 40, pos: 'top'},
+      {name: 'R108', x: 145, y: 160, xdim: 55, ydim: 40, isHorizontal: false , pos: 'left'},
     ];
   }
 }
